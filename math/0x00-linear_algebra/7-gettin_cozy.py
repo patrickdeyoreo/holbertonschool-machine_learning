e51d6af1bf5234f97fddfb3aec84265659f07a6f
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 00:32:38 -0700

    Checking matrix compatibility

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,6 +5,10 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenates two 2D matrices"""
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return [u.copy() for u in mat1] + [v.copy() for v in mat2]
     else:
+        if len(mat1) != len(mat2):
+            return None
         return [u + v for u, v in zip(mat1, mat2)]
