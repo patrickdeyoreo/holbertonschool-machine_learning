e693c3ca2b9395d20cb6f52b62ddecf97e166c8d
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 15:30:13 -0700

    Adding elegant solution to task 14

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Provides a function to concatenate two 2-dimensional matrices"""
+"""Provides a function to concatenate two numpy.ndarray's"""
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """Concatenates two 2D matrices along the specified axis"""
+    """Concatenates two numpy.ndarray's along the specified axis"""
     return np.concatenate((mat1, mat2), axis=axis)
---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Provides a function to multiply two numpy.ndarray's"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication of two numpy.ndarray's"""
+    return mat1 @ mat2
