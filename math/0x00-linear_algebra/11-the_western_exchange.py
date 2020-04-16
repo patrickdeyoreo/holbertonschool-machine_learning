f0cd51aa87d562ce088ba48886903215d82bf0b0
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 15:04:41 -0700

    Adding ridiculous solution to task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Provides a function to compute the transpose of a numpy.ndarray"""
+
+
+def np_transpose(matrix):
+    """Computes the transpose of a numpy.ndarray"""
+    return matrix.T
