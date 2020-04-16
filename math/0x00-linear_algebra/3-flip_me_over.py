a3e6f2d734754158630c783e75741e0014a4825c
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 15:37:05 -0700

    One-liner for task 3

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -4,8 +4,4 @@
 
 def matrix_transpose(matrix):
     """Computes the transpose of a 2D matrix"""
-    trans = [[None] * len(matrix) for _ in range(len(matrix[0]))]
-    for row in range(len(trans)):
-        for col in range(len(trans[0])):
-            trans[row][col] = matrix[col][row]
-    return trans
+    return list(zip(*matrix))
