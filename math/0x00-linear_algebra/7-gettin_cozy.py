d099273e42658416772c8e1460b0fbaaa0dda6d4
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 00:28:26 -0700

    Removed type hints from task 7

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Provides a function to concatenate two 2-dimensional matrices"""
-from typing import List
 
 
-def cat_matrices2D(mat1: List, mat2: List, axis: int = 0) -> List[List[int]]:
+def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenates two 2D matrices"""
     if axis == 0:
         return [u.copy() for u in mat1] + [v.copy() for v in mat2]
