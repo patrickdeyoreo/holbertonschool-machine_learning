0586a14623d3c7f52aed63e503267e1339ea17e0
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 11:29:32 -0700

    Using subtraction to slice middle of array instead of mod

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for row in matrix:
-    the_middle.append(row[len(row)//2:len(row)//2 + (len(row)+1) % 2 + 1])
+    the_middle.append(row[(len(row) - 1) // 2:len(row) // 2 + 1])
 print("The middle columns of the matrix are: {}".format(the_middle))
