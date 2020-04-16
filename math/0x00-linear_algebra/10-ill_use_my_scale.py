a0c968daafc79897fe48fe2a2648d3beacea7b13
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 14:34:51 -0700

    Adding ridiculous solution to task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Provides a function to compute the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Computes the shape of a numpy.ndarray"""
+    return matrix.shape
