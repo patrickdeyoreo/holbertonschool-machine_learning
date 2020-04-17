ec907138c02d04b5bf1588374c45a8e860b9b1a7
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-16 22:29:47 -0700

    Handling shape mismatch in task 16

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -4,6 +4,16 @@
 
 def add_matrices(mat1, mat2):
     """Performs element-wise addition of two n-dimensional matrices"""
+    try:
+        return add_matrices_map(mat1, mat2)
+    except ValueError:
+        return None
+
+
+def add_matrices_map(mat1, mat2):
+    """Recursive map wrapper to add two n-dimensional matrices"""
+    if len(mat1) != len(mat2):
+        raise ValueError
     if isinstance(mat1[0], list) and isinstance(mat2[0], list):
-        return list(map(add_matrices, mat1, mat2))
+        return list(map(add_matrices_map, mat1, mat2))
     return [a + b for a, b in zip(mat1, mat2)]
