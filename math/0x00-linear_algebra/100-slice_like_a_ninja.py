50f756f74f2cc859cce5f8e07868c8f849494560
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-17 01:54:52 -0700

    removed unused numpy import from task 15

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Provides a function to slice a numpy.ndarray along specific axes"""
-import numpy as np
 
 
 def np_slice(matrix, axes={}):
     """Slices a numpy.ndarray along specific axes"""
-    part = [slice(*axes.get(depth, (None, None, None)))
-            for depth, length in enumerate(matrix.shape)]
+    part = (slice(*axes.get(depth, (None, None, None)))
+            for depth, length in enumerate(matrix.shape))
     return matrix[tuple(part)]
