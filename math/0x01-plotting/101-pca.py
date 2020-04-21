e9d5672b29d367e1d8a15b7b2c83e593848eff25
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-21 00:16:23 -0700

    Adding solution to task 8 (PCA)

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -12,4 +12,13 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-# your code here
+fig = plt.figure()
+fig.suptitle("PCA of Iris Dataset")
+
+axs = fig.add_subplot(111, projection='3d')
+axs.set_xlabel('U1')
+axs.set_ylabel('U2')
+axs.set_zlabel('U3')
+axs.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2], c=labels)
+
+plt.show()
