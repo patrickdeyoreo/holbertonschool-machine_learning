e4dc8a73c28529f181ce856d31865dd7d4021d8a
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-21 10:35:32 -0700

    Reordered steps in task 7

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -10,11 +10,12 @@ z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
 gradient = 'summer'
 
+fig, axs = plt.subplots()
+fig.suptitle("Mountain Elevation")
+
 scm = plt.cm.ScalarMappable(cmap=plt.get_cmap(gradient))
 scm.set_clim([0, z.max()])
 
-fig, axs = plt.subplots()
-fig.suptitle("Mountain Elevation")
 fig.colorbar(scm, label="elevation (m)")
 
 axs.scatter(x, y, c=z, cmap=scm.get_cmap())
