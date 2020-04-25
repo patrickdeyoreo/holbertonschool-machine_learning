1f89804df6552d839789e0aeebac5fd4acb4253f
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-25 00:31:27 -0700

    Added a few tests to task 17

--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -4,3 +4,9 @@ poly_integral = __import__('17-integrate').poly_integral
 
 poly = [5, 3, 0, 1]
 print(poly_integral(poly))
+poly = [0, 0]
+print(poly_integral(poly))
+poly = []
+print(poly_integral(poly))
+poly = 10
+print(poly_integral(poly))
