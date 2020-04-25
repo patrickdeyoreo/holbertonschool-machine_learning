6cf70cdc8d434c32e7a1d3ea11b9d55aa657ce01
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-24 18:53:16 -0700

    Handling leading zeros in task 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,10 +4,15 @@
 
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
-    res = [C]
     try:
-        res += [coeff / power if coeff / power % 1 else coeff // power
+        poly = poly[:]
+        while poly[-1] == 0:
+            poly.pop()
+        poly = [coeff / power if coeff / power % 1 else coeff // power
                 for power, coeff in enumerate(poly, 1)]
+        poly.insert(0, C)
+    except IndexError:
+        poly = [C]
     except (TypeError, ValueError):
-        return None
-    return res
+        poly = None
+    return poly
