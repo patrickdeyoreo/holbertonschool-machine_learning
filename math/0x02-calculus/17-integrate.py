d3c76cde299aad8111890227561b598c34df0e21
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-25 00:08:59 -0700

    Treating empty list as invalid input in task 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,14 +5,14 @@
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
     try:
-        poly = poly[:]
-        while poly[-1] == 0:
-            poly.pop()
-        poly = [coeff / power if coeff / power % 1 else coeff // power
-                for power, coeff in enumerate(poly, 1)]
-        poly.insert(0, C)
+        integral = poly[:]
+        while integral[-1] == 0:
+            integral.pop()
+        integral = [coeff / power if coeff / power % 1 else coeff // power
+                    for power, coeff in enumerate(integral, 1)]
+        integral.insert(0, C)
+        return integral
     except IndexError:
-        poly = [C]
+        return None if len(poly) == 0 else [C]
     except (TypeError, ValueError):
-        poly = None
-    return poly
+        return None
