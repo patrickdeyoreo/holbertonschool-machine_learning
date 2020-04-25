787f1deeae1cfce64305994b5c1bdff90332452b
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-25 00:58:03 -0700

    Invalidate negative n and use try-except in task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,7 @@
 
 def summation_i_squared(n):
     """Calculates the summation of i^2 from i=1 to n"""
-    if isinstance(n, int):
-        return sum(map(lambda i: i ** 2, range(1, n + n ** 0, n ** 0)))
-    return None
+    try:
+        return sum(map(lambda i: i ** 2, range(1, n + 1))) or None
+    except (TypeError, ValueError):
+        return None
