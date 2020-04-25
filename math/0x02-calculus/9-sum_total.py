acabde3e7b16c7ae5d41a3a81294e541fc80d95d
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-25 00:49:31 -0700

    Use map in task 9 because I don't want to recurse

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """Calculates the summation of i^2 from i=1 to n"""
     try:
-        return sum(i ** 2 for i in range(1, n + 1))
+        return sum(map(lambda i: i ** 2, range(1, n + 1)))
     except (TypeError, ValueError):
         return None
