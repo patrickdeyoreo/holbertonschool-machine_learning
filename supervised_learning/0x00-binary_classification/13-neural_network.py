Fixed sigmoid derivative in gradient descent

supervised_learning/0x00-binary_classification/13-neural_network.py
@@ -130,12 +130,13 @@ class NeuralNetwork:
             A2: the predicted output
             alpha: the learning rate
         """
+        m = X.shape[1]
         dZ2 = A2 - Y
-        dW2 = (dZ2 @ A1.T) / X.shape[1]
-        db2 = np.sum(dZ2, axis=1, keepdims=True) / X.shape[1]
-        dZ1 = (self.W2.T @ dZ2) * (A1 - (1 / A1))
-        dW1 = (dZ1 @ X.T) / X.shape[1]
-        db1 = np.sum(dZ1, axis=1, keepdims=True) / X.shape[1]
+        dW2 = (1 / m) * (dZ2 @ A1.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+        dZ1 = (self.W2.T @ dZ2) * (A1 * (1 - A1))
+        dW1 = (1 / m) * (dZ1 @ X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
         self.__W1 -= alpha * dW1
         self.__b1 -= alpha * db1
         self.__W2 -= alpha * dW2