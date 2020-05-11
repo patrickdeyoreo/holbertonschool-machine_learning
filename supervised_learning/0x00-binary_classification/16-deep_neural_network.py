e35bc946133cc307ea24c46965091414298a15d1
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-05-10 18:31:29 -0700

    Checking length of list in task 16

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -21,7 +21,7 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if isinstance(layers, list) is False:
+        if isinstance(layers, list) is False or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
