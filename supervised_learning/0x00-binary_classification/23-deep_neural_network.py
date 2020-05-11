a93c980151eafb7cff7d94571dad1c3582362a0b
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-05-10 21:19:37 -0700

    Added task 23 and updated task 14

--- supervised_learning/0x00-binary_classification/23-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/23-deep_neural_network.py
@@ -1,95 +1,72 @@
 #!/usr/bin/env python3
-"""Provides a class ``NeuralNetwork'' for binary classification"""
+"""Provides a class ``DeepNeuralNetwork'' for binary classification"""
 # pylint: disable=invalid-name
 
 import numpy as np
 
 
-class NeuralNetwork:
-    """Represents a binary classification network with one hidden layer"""
+class DeepNeuralNetwork:
+    """Represents a deep neural network for binary classification"""
 
-    def __init__(self, nx, nodes):
+    def __init__(self, nx, layers):
         """
         Initializes a binary classification neuron
         Arguments:
             nx: the number of input features
-            nodes: the number of nodes found in the hidden layer
+            layers: a list representing the number of nodes in each layer
         """
-        if not isinstance(nx, int):
+        if isinstance(nx, int) is False:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(nodes, int):
-            raise TypeError("nodes must be an integer")
-        if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
+        if isinstance(layers, list) is False or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
 
-        self.__W1 = np.random.randn(nodes, nx)
-        self.__b1 = np.zeros((nodes, 1))
-        self.__A1 = 0
-        self.__W2 = np.random.randn(1, nodes)
-        self.__b2 = 0
-        self.__A2 = 0
-
-    @property
-    def W1(self):
-        """
-        Get the weights vector of the hidden layer
-        Return:
-            the weights vector
-        """
-        return self.__W1
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        d1 = nx
+        for index, d0 in enumerate(layers, 1):
+            if isinstance(d0, int) is False or d0 < 1:
+                raise TypeError("layers must be a list of positive integers")
+            key = 'W{}'.format(index)
+            self.__weights[key] = np.random.randn(d0, d1) * np.sqrt(2 / d1)
+            key = 'b{}'.format(index)
+            self.__weights[key] = np.zeros((d0, 1))
+            d1 = d0
 
     @property
-    def b1(self):
+    def L(self):
         """
-        Get the bias vector of the hidden layer
+        Get the number of layers
         Return:
-            the bias
+            the number of layers
         """
-        return self.__b1
+        return self.__L
 
     @property
-    def A1(self):
+    def cache(self):
         """
-        Get the activation state of the hidden layer
+        Get the intermediary values of the network
         Return:
-            the activation state
+            the cache dictionary
         """
-        return self.__A1
+        return self.__cache
 
     @property
-    def W2(self):
+    def weights(self):
         """
-        Get the weights vector of the output layer
+        Get the weights and biases of the network
         Return:
-            the weights vector
+            the weights dictionary
         """
-        return self.__W2
-
-    @property
-    def b2(self):
-        """
-        Get the bias vector of the output layer
-        Return:
-            the bias
-        """
-        return self.__b2
-
-    @property
-    def A2(self):
-        """
-        Get the activation state of the output layer
-        Return:
-            the activation state
-        """
-        return self.__A2
+        return self.__weights
 
     def forward_prop(self, X):
         """
         Calculates the forward propagation of the network using a sigmoid
-        activation function and updates the activation state
+        activation function and updates the cache
         Arguments:
             X: numpy.ndarray with shape (nx, m) that contains the input, where
                nx is the number of input features to the neuron, and
@@ -97,9 +74,12 @@ class NeuralNetwork:
         Return:
             the activation state
         """
-        self.__A1 = self.sigmoid(self.W1 @ X + self.b1)
-        self.__A2 = self.sigmoid(self.W2 @ self.A1 + self.b2)
-        return (self.A1, self.A2)
+        self.__cache['A0'] = X
+        for index in range(1, self.L + 1):
+            W = self.weights['W{}'.format(index)]
+            b = self.weights['b{}'.format(index)]
+            X = self.__cache['A{}'.format(index)] = self.sigmoid(W @ X + b)
+        return (X, self.cache)
 
     def evaluate(self, X, Y):
         """
@@ -112,35 +92,29 @@ class NeuralNetwork:
         Return:
             the neuron’s prediction and the cost of the network, respectively
         """
-        _, A2 = self.forward_prop(X)
-        P = np.where(A2 < 0.5, 0, 1)
-        c = self.cost(Y, A2)
+        A = self.forward_prop(X)[0]
+        P = np.where(A < 0.5, 0, 1)
+        c = self.cost(Y, A)
         return (P, c)
 
-    # pylint: disable=too-many-arguments
-    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+    def gradient_descent(self, Y, cache, alpha=0.05):
         """
         Calculates a pass of gradient descent and updates the weights and bias
         Arguments:
-            X: numpy.ndarray with shape (nx, m) that contains the input, where
-               nx is the number of input features to the neuron, and
-               m is the number of examples
             Y: numpy.ndarray with shape (1, m) that contains the correct labels
-            A1: the output of the hidden layer
-            A2: the predicted output
+            cache: dictionary containing the intermediary values of the network
             alpha: the learning rate
         """
-        m = X.shape[1]
-        dZ2 = A2 - Y
-        dW2 = (1 / m) * (dZ2 @ A1.T)
-        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
-        dZ1 = (self.W2.T @ dZ2) * (A1 * (1 - A1))
-        dW1 = (1 / m) * (dZ1 @ X.T)
-        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
-        self.__W1 -= alpha * dW1
-        self.__b1 -= alpha * db1
-        self.__W2 -= alpha * dW2
-        self.__b2 -= alpha * db2
+        m = Y.shape[1]
+        dZ = cache['A{}'.format(self.L)] - Y
+        for index in reversed(range(self.L)):
+            A = cache['A{}'.format(index)]
+            W = self.weights['W{}'.format(index + 1)]
+            dW = (1 / m) * (dZ @ A.T)
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+            dZ = (W.T @ dZ) * (A * (1 - A))
+            self.__weights['W{}'.format(index + 1)] -= alpha * dW
+            self.__weights['b{}'.format(index + 1)] -= alpha * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """
@@ -166,8 +140,8 @@ class NeuralNetwork:
 
         iteration = 0
         while iteration < iterations:
-            A1, A2 = self.forward_prop(X)
-            self.gradient_descent(X, Y, A1, A2, alpha)
+            self.forward_prop(X)
+            self.gradient_descent(Y, self.cache, alpha)
             iteration += 1
         return self.evaluate(X, Y)
 
