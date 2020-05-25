ced69d85706ffadb628ae6162ed726c8126f3bc8
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-05-24 21:12:33 -0700

    Adding tasks 0-3

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -9,11 +9,11 @@ shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def train_mini_batch(
-    X_train, Y_train,
-    X_valid, Y_valid,
-    batch_size=32, epochs=5,
-    load_path="/tmp/model.ckpt",
-    save_path="/tmp/model.ckpt",
+        X_train, Y_train,
+        X_valid, Y_valid,
+        batch_size=32, epochs=5,
+        load_path="/tmp/model.ckpt",
+        save_path="/tmp/model.ckpt",
 ):
     """
     Trains neural network using mini-batch gradient descent
