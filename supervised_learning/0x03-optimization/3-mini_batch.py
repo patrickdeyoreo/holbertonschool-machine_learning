72c6ec840424802fe4bfeddbc3c979555dc0d8ce
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-05-24 21:40:50 -0700

    Using str.format to print floating point numbers

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -30,73 +30,70 @@ def train_mini_batch(
         the path at which the model was saved
     """
     with tf.Session() as session:
-        saver = tf.train.import_meta_graph(load_path + '.meta')
-        saver.restore(session, load_path)
-        var = {
-            'x': None,
-            'y': None,
-            'accuracy': None,
-            'loss': None,
-            'train_op': None,
-            'y_pred': None
-        }
-        for name in var:
-            var[name] = tf.get_collection(name)[0]
 
-        epoch = 0
+        saver = tf.train.import_meta_graph('.'.join((load_path, 'meta')))
+        saver.restore(session, load_path)
+
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        train_op = tf.get_collection('train_op')[0]
+
         for epoch in range(epochs + 1):
             loss_t = session.run(
-                var['loss'],
-                feed_dict={var['x']: X_train, var['y']: Y_train})
+                loss,
+                feed_dict={x: X_train, y: Y_train})
             accuracy_t = session.run(
-                var['accuracy'],
-                feed_dict={var['x']: X_train, var['y']: Y_train})
+                accuracy,
+                feed_dict={x: X_train, y: Y_train})
             loss_v = session.run(
-                var['loss'],
-                feed_dict={var['x']: X_valid, var['y']: Y_valid})
+                loss,
+                feed_dict={x: X_valid, y: Y_valid})
             accuracy_v = session.run(
-                var['accuracy'],
-                feed_dict={var['x']: X_valid, var['y']: Y_valid})
-            print("After", epoch, "epochs:")
-            print("\tTraining Cost:", loss_t)
-            print("\tTraining Accuracy:", accuracy_t)
-            print("\tValidation Cost:", loss_v)
-            print("\tValidation Accuracy:", accuracy_v)
+                accuracy,
+                feed_dict={x: X_valid, y: Y_valid})
 
-            if epoch < epochs:
-                X_perm, Y_perm = shuffle_data(X_train, Y_train)
-                batches, remainder = divmod(X_train.shape[0], batch_size)
+            print("After {} epochs:".format(epoch))
+            print("\tTraining Cost: {}".format(loss_t))
+            print("\tTraining Accuracy: {}".format(accuracy_t))
+            print("\tValidation Cost: {}".format(loss_v))
+            print("\tValidation Accuracy: {}".format(accuracy_v))
 
-                descent_step = 0
-                for i in range(0, batches + 1):
-                    descent_step += 1
+            if epoch == epochs:
+                break
 
-                    # Important: make copies of X_perm and Y_perm
-                    if i == batches:
-                        if remainder != 0:
-                            X_batch = X_perm[i * batch_size:]
-                            Y_batch = Y_perm[i * batch_size:]
-                        else:
-                            break
-                    else:
-                        X_batch = X_perm[i * batch_size:(i + 1) * batch_size]
-                        Y_batch = Y_perm[i * batch_size:(i + 1) * batch_size]
+            X_perm, Y_perm = shuffle_data(X_train, Y_train)
+            batches, rem = divmod(X_train.shape[0], batch_size)
 
-                    session.run(
-                        var['train_op'],
-                        feed_dict={var['x']: X_batch, var['y']: Y_batch})
+            step = 0
+            for bat in range(batches + 1):
+                step += 1
 
-                    if descent_step % 100 == 0:
-                        loss_b = session.run(
-                            var['loss'],
-                            feed_dict={var['x']: X_batch, var['y']: Y_batch})
-                        accuracy_b = session.run(
-                            var['accuracy'],
-                            feed_dict={var['x']: X_batch, var['y']: Y_batch})
+                if bat == batches:
+                    if rem == 0:
+                        break
+                    X_bat = X_perm[bat * batch_size:]
+                    Y_bat = Y_perm[bat * batch_size:]
+                else:
+                    X_bat = X_perm[bat * batch_size:(bat + 1) * batch_size]
+                    Y_bat = Y_perm[bat * batch_size:(bat + 1) * batch_size]
 
-                        print("\tStep", descent_step, end=":\n")
-                        print("\t\tCost:", loss_b)
-                        print("\t\tAccuracy:", accuracy_b)
+                session.run(
+                    train_op,
+                    feed_dict={x: X_bat, y: Y_bat})
+
+                if step % 100 == 0:
+                    loss_b = session.run(
+                        loss,
+                        feed_dict={x: X_bat, y: Y_bat})
+                    accuracy_b = session.run(
+                        accuracy,
+                        feed_dict={x: X_bat, y: Y_bat})
+
+                    print("\tStep {}:".format(step))
+                    print("\t\tCost: {}".format(loss_b))
+                    print("\t\tAccuracy: {}".format(accuracy_b))
 
         save_path = saver.save(session, save_path)
     return save_path
