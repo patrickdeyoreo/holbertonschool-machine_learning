47af36f4e9369d7551b31b51cd2f1bdc0e36b295
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-04 13:39:39 -0700

    Fixed typo in function name

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -6,16 +6,15 @@ Provides functions to save and load model config
 import tensorflow.keras as K
 
 
-def save_cofig(network, filename):
+def save_config(network, filename):
     """
     Saves the configuration of a model as JSON
     Arguments:
         network: the model of which to save the config
         filename: the path at which to save the config
     """
-    config = network.to_json()
     with open(filename, 'w') as ostream:
-        ostream.write(config)
+        ostream.write(network.to_json())
 
 
 def load_config(filename):
