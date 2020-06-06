7a3d17b0a994f895a5ffb81255c65d4f7748f97f
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-06 02:45:06 -0700

    Adding task 5

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 """
-Provides a function to perform a convolution on images
+Provides a function to perform convolutions on images
 """
 # pylint: disable=invalid-name
 import numpy as np
 
 
-def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+def convolve(images, kernels, padding='same', stride=(1, 1)):
     """
-    Performs a convolution on images
+    Performs convolutions on images
     Arguments:
         images:
-            a numpy.ndarray of shape (m, h, w, c) containing images, where
+            a np.ndarray of shape (m, h, w, c) containing images, where
             m is the number of images,
             h is the height in pixels of each image,
             w is the width in pixels of each image,
             c is the number of channels in each image
         kernel:
-            a numpy.ndarray of shape (hk, wk, c) containing the kernel, where
-            hk is the height in pixels of the kernel,
-            wk is the width in pixels of the kernel,
-            c is the number of channels in each image
+            a np.ndarray of shape (hk, wk, c, n) containing the kernels, where
+            hk is the height in pixels of each kernel,
+            wk is the width in pixels of each kernel,
+            c is the number of channels in each image,
+            n is the number of kernels
         padding:
             either ‘same’, ‘valid’, or a tuple of (hp, wp), where
             ‘same’ performs a same convolution,
@@ -32,9 +33,11 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
             hs is the height of the stride for each image,
             ws is the width of the stride for each image
     Return:
-        a numpy.ndarray containing the convolved images
+        a np.ndarray containing the convolved images
     """
-    (m, h, w, _), (hk, wk, _), (hs, ws) = images.shape, kernel.shape, stride
+    m, h, w, _ = images.shape
+    hk, wk, _, n = kernels.shape
+    hs, ws = stride
 
     if isinstance(padding, tuple):
         hp, wp = padding
@@ -47,11 +50,12 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     images = np.pad(images, ((0,), (hp,), (wp,), (0,)), mode='constant')
     h = (h - hk + 2 * hp) // hs + 1
     w = (w - wk + 2 * wp) // ws + 1
-    convolved = np.zeros(shape=(m, h, w))
-    for row in range(h):
-        for col in range(w):
-            rows = slice(row * hs, row * hs + hk)
-            cols = slice(col * ws, col * ws + wk)
-            part = images[:, rows, cols] * kernel
-            convolved[:, row, col] = np.sum(part, axis=(1, 2, 3))
+    convolved = np.zeros(shape=(m, h, w, n))
+    for kern in range(n):
+        for row in range(h):
+            for col in range(w):
+                rows = slice(row * hs, row * hs + hk)
+                cols = slice(col * ws, col * ws + wk)
+                part = images[:, rows, cols] * kernels[:, :, :, kern]
+                convolved[:, row, col, kern] = np.sum(part, axis=(1, 2, 3))
     return convolved
