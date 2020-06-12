372d038cd6b7d51dbaccb38b5ccab7f28e10a5a8
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-11 19:51:58 -0700

    Adding task 1

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -9,10 +9,9 @@ import numpy as np
 
 def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
     """
-    Performs convolutions on images
+    Performs forward prop over a convolutional layer of a neural network
     Arguments:
-        A_prev: np.ndarray of shape (m, h_i, w_i, c_i) containing
-                the output of the previous layer, where
+        A_prev: np.ndarray of shape (m, h_i, w_i, c_i) containing input, where
                 m is the number of examples,
                 h_i is the height of the previous layer,
                 w_i is the width of the previous layer,
@@ -25,7 +24,7 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
         b: np.ndarray of shape (1, 1, 1, c_o) containing biases
         activation: an activation function to apply to the convolution
         padding: either 'same' or 'valid', indicating the type of padding
-        stride: a tuple (h_s, w_s) of the strides for the convolution, where
+        stride: a tuple (h_s, w_s) specifying the stride size, where
                 h_s is the height of the stride,
                 w_s is the width of the stride
     Return:
@@ -35,13 +34,12 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
     m, h_i, w_i, _ = A_prev.shape
     h_k, w_k, _, c_o = W.shape
     h_s, w_s = stride
-    if padding == 'same':
+    if padding.lower() == 'valid':
         h_p = ((h_s - 1) * h_i - h_s + h_k + 1) // 2
         w_p = ((w_s - 1) * w_i - w_s + w_k + 1) // 2
     else:
         h_p = w_p = 0
-    pad_width = ((0,), (h_p,), (w_p,), (0,))
-    A_prev = np.pad(A_prev, pad_width, mode='constant')
+    A_prev = np.pad(A_prev, ((0,), (h_p,), (w_p,), (0,)), mode='constant')
     h_o = (h_i - h_k + 2 * h_p) // h_s + 1
     w_o = (w_i - w_k + 2 * w_p) // w_s + 1
     conv = np.zeros(shape=(m, h_o, w_o, c_o))
@@ -53,4 +51,4 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
                 rows = slice(i * h_s, i * h_s + h_k)
                 part = A_prev[:, rows, cols]
                 conv[:, i, j, k] = np.sum(part * kern, axis=(1, 2, 3)) + b
-    return conv if activation is None else activation(conv)
+    return conv if not callable(activation) else activation(conv)
---
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Provides a function to perform forward propagation over a pooling layer
+of a neural network
+"""
+# pylint: disable=invalid-name
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """
+    Performs forward prop over a pooling layer of a neural network
+    Arguments:
+        A_prev: np.ndarray of shape (m, h_i, w_i, c_i) containing input, where
+                m is the number of examples,
+                h_i is the height of the previous layer,
+                w_i is the width of the previous layer,
+                c is the number of channels in the previous layer
+        kernel_shape: a tuple (h_k, w_k) specifiying the kernel size, where
+                h_k is the filter height,
+                w_k is the filter width,
+        stride: a tuple (h_s, w_s) specifying the stride size, where
+                h_s is the height of the stride,
+                w_s is the width of the stride
+        max: either 'max' or 'avg', indicating the type of pooling
+    Return:
+        the output of the pooling layer
+    """
+    # pylint: disable=too-many-arguments,too-many-locals
+    m, h_i, w_i, c = A_prev.shape
+    h_k, w_k = kernel_shape
+    h_s, w_s = stride
+    h_o = (h_i - h_k) // h_s + 1
+    w_o = (w_i - w_k) // w_s + 1
+    poolfn = np.mean if mode.lower() == 'avg' else np.max
+    pooled = np.zeros(shape=(m, h_o, w_o, c))
+    for i in range(h_o):
+        rows = slice(i * h_s, i * h_s + h_k)
+        for j in range(w_o):
+            cols = slice(j * w_s, j * w_s + w_k)
+            part = A_prev[:, rows, cols]
+            pooled[:, i, j] = poolfn(part, axis=(1, 2))
+    return pooled
