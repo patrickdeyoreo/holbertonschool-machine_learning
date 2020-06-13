cb12222474ddcf133d1aba92a006ec826ee682c1
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-13 00:32:49 -0700

    Adding CNN backprop

---
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Provides a function to perform back propagation over a convolutional layer
+of a neural network
+"""
+# pylint: disable=invalid-name
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding='same', stride=(1, 1)):
+    """
+    Performs back prop over a convolutional layer of a neural network
+    Arguments:
+        dZ: np.ndarray of shape (m, h_o, w_o, c_o) containing the partial
+                derivatives with respect to the unactivated output, where
+                m is the number of examples,
+                h_o is the height of the output,
+                w_o is the width of the output,
+                c_o is the number of channels in the output
+        A_prev: np.ndarray of shape (m, h_i, w_i, c_i) containing input, where
+                m is the number of examples,
+                h_i is the height of the previous layer,
+                w_i is the width of the previous layer,
+                c_i is the number of channels in the previous layer
+        W: np.ndarray of shape (h_k, w_k, c_i, c_o) containing kernels, where
+                h_k is the filter height,
+                w_k is the filter width,
+                c_i is the number of channels in the previous layer,zeros
+                c_o is the number of channels in the output
+        b: np.ndarray of shape (1, 1, 1, c_o) containing biases, where
+                c_o is the number of channels in the output
+        padding: a string, either 'same' or 'valid', where
+                'same' specifies a same convolution,
+                'valid' specifies a valid convolution
+        stride: a tuple (h_s, w_s) specifying the stride size, where
+                h_s is the height of the stride,
+                w_s is the width of the stride
+    Return:
+        the partial derivatives with respect to the previous layer (dA_prev),
+        the kernels (dW), and the biases (db), respectively
+    """
+    # pylint: disable=too-many-arguments,too-many-locals
+
+    _, h_o, w_o, c_o = dZ.shape
+    _, h_i, w_i, _ = A_prev.shape
+    h_k, w_k, _, _ = W.shape
+    h_s, w_s = stride
+
+    if padding == 'same':
+        h_p = ((h_s - 1) * h_i - h_s + h_k + 1) // 2
+        w_p = ((w_s - 1) * w_i - w_s + w_k + 1) // 2
+    else:
+        h_p = w_p = 0
+
+    pad_width = ((0,), (h_p,), (w_p,), (0,))
+    A_prev = np.pad(A_prev, pad_width, mode='constant')
+
+    dA_prev = np.zeros(A_prev.shape)
+    dW = np.zeros(W.shape)
+    db = np.zeros(b.shape)
+
+    for row in range(h_o):
+        rows = slice(row * h_s, row * h_s + h_k)
+        for col in range(w_o):
+            cols = slice(col * w_s, col * w_s + w_k)
+            A = A_prev[:, rows, cols]
+            for kern in range(c_o):
+                K = W[:, :, :, kern]
+                X = dZ[:, row:row+1, col:col+1, kern:kern+1]
+                dW[:, :, :, kern] += np.sum(X * A, axis=0)
+                db[:, :, :, kern] += np.sum(X)
+                dA_prev[:, rows, cols] += K[np.newaxis, ...] * X
+
+    return (dA_prev, dW, db)
