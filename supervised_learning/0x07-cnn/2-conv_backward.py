bba33b3f3d4fe5d861fe5ade952f7d164d785ece
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-15 22:25:08 -0700

    Same backprop matches valid backprop

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -11,24 +11,24 @@ def conv_backward(dZ, A_prev, W, b, padding='same', stride=(1, 1)):
     """
     Performs back prop over a convolutional layer of a neural network
     Arguments:
-        dZ: np.ndarray of shape (m, h, w, c) containing the partial
+        dZ: np.ndarray of shape (m, h_o, w_o, c_o) containing the partial
                 derivatives with respect to the unactivated output, where
                 m is the number of examples,
-                h is the height of the output,
-                w is the width of the output,
-                c is the number of channels in the output
+                h_o is the height of the output,
+                w_o is the width of the output,
+                c_o is the number of channels in the output
         A_prev: np.ndarray of shape (m, h_i, w_i, c_i) containing input, where
                 m is the number of examples,
                 h_i is the height of the previous layer,
                 w_i is the width of the previous layer,
                 c_i is the number of channels in the previous layer
-        W: np.ndarray of shape (h_k, w_k, c_i, c) containing kernels, where
+        W: np.ndarray of shape (h_k, w_k, c_i, c_o) containing kernels, where
                 h_k is the filter height,
                 w_k is the filter width,
                 c_i is the number of channels in the previous layer,
-                c is the number of channels in the output
-        b: np.ndarray of shape (1, 1, 1, c) containing biases, where
-                c is the number of channels in the output
+                c_o is the number of channels in the output
+        b: np.ndarray of shape (1, 1, 1, c_o) containing biases, where
+                c_o is the number of channels in the output
         padding: 'same' or 'valid', indicating the type of convolution, where
                 'same' specifies a same convolution,
                 'valid' specifies a valid convolution
@@ -41,28 +41,29 @@ def conv_backward(dZ, A_prev, W, b, padding='same', stride=(1, 1)):
     """
     # pylint: disable=too-many-arguments,too-many-locals
 
-    m, h, w, c = dZ.shape
+    m, h_o, w_o, c_o = dZ.shape
     _, h_i, w_i, _ = A_prev.shape
     h_k, w_k, _, _ = W.shape
     h_s, w_s = stride
 
     if padding == 'same':
-        h_p = ((h_s - 1) * h_i - h_s + h_k + 1) // 2
-        w_p = ((w_s - 1) * w_i - w_s + w_k + 1) // 2
+        h_p = ((h_o - 1) * h_s - h_i + h_k) // 2
+        w_p = ((w_o - 1) * w_s - w_i + w_k) // 2
     else:
         h_p = w_p = 0
 
-    A_prev = np.pad(A_prev, ((0,), (h_p,), (w_p,), (0,)), mode='constant')
+    pad_width = ((0,), (h_p,), (w_p,), (0,))
+    A_prev = np.pad(A_prev, pad_width, mode='constant')
 
     dX = np.zeros(A_prev.shape)
     dW = np.zeros(W.shape)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    for kern in range(c):
+    for kern in range(c_o):
         K = W[..., kern]
-        for row in range(h):
+        for row in range(h_o):
             rows = slice(row * h_s, row * h_s + h_k)
-            for col in range(w):
+            for col in range(w_o):
                 cols = slice(col * w_s, col * w_s + w_k)
                 for img in range(m):
                     A = A_prev[img, rows, cols]
