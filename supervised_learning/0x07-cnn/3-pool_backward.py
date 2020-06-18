382427b518cbb343c2d9f33ba0dc4eaa67d08e41
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-17 23:40:56 -0700

    Fixed typos in task 3

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -9,12 +9,12 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Perform back-propagation over a pooling layer of a neural network.
 
     Arguments:
-        dA: np.ndarray of shape (m, h, w, c) containing the partial
+        dA: np.ndarray of shape (m, h_o, w_o, c_o) containing the partial
                 derivatives with respect to the pooled output, where
                 m is the number of examples,
-                h is the height of the output,
-                w is the width of the output,
-                c is the number of channels in the output
+                h_o is the height of the output,
+                w_o is the width of the output,
+                c_o is the number of channels in the output
         A_prev: np.ndarray of shape (m, h_i, w_i, c_i) containing input, where
                 m is the number of examples,
                 h_i is the height of the previous layer,
@@ -34,20 +34,27 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     # pylint: disable=too-many-arguments,too-many-locals
 
-    _, h, w, _ = dA.shape
+    m, h_o, w_o, c_o = dA.shape
     h_k, w_k = kernel_shape
     h_s, w_s = stride
-    
-    mask = {'max': lambda A: np.where(A == np.max(A), 1, 0),
-            'avg': lambda A: np.ones(A.shape) / (h_k * w_k)}[mode]
+
+    if mode == 'max':
+        def mask(A):
+            """Mask for max pooling."""
+            return np.where(A < np.max(A), 0, 1)
+    else:
+        def mask(A):
+            """Mask for average pooling."""
+            return np.ones_like(A) / (h_k * w_k)
+
     dX = np.zeros(A_prev.shape)
 
     for kern in range(c_o):
         for row in range(h_o):
             rows = slice(row * h_s, row * h_s + h_k)
-            for col in range(w_o
+            for col in range(w_o):
                 cols = slice(col * w_s, col * w_s + w_k)
                 for img in range(m):
-                    X = A_prev[img, rows, cols]
-                    dX[img, rows, cols] += mask(X) * dA
+                    X = mask(A_prev[img, rows, cols, kern])
+                    dX[img, rows, cols, kern] += X * dA[img, row, col, kern]
     return dX
