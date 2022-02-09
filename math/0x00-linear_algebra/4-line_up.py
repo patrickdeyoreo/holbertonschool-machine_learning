c01546bb4f15840f99f8dd71d6ff91c31a008cfe
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-09 13:15:12 -0800

    Reformated task 4

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -7,6 +7,6 @@ def add_arrays(arr1: list, arr2: list) -> list:
     """
     Add two arrays element-wise.
     """
-    if len(arr1) != len(arr2):
-        return None
-    return [a + b for a, b in zip(arr1, arr2)]
+    if len(arr1) == len(arr2):
+        return [a + b for a, b in zip(arr1, arr2)]
+    return None
