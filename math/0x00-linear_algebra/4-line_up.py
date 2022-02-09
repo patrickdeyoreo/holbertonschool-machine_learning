1d82335061c20fe3bd5c2b4a010d4a8d70323bc8
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-09 13:13:41 -0800

    Tested type annotations

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -3,7 +3,7 @@
 Provide a function to add two arrays element-wise.
 """
 
-def add_arrays(arr1: list[int], arr2: list[int]) -> list[int]:
+def add_arrays(arr1: list, arr2: list) -> list:
     """
     Add two arrays element-wise.
     """
