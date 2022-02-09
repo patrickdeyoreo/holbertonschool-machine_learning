b8f4fdf28063b88f0ccd6c6e4f73c1efd8e09333
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-09 13:09:35 -0800

    Added task 4

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Provide a function to add two arrays element-wise.
+"""
+
+def add_arrays(arr1: list[int], arr2: list[int]) -> list[int]:
+    """
+    Add two arrays element-wise.
+    """
+    if len(arr1) != len(arr2):
+        return None
+    return [a + b for a, b in zip(arr1, arr2)]
