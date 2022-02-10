e5df35245935c7536948a5d562916c1bde45c187
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-10 14:11:02 -0800

    Added task 14

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Provide a function to perform matrix multiplication of two numpy arrays.
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Perform matrix multiplication of two numpy arrays.
+    """
+    return mat1 @ mat2
+
