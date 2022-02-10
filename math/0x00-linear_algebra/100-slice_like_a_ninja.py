a3233d51167ba8986770183078a2477acb1b44bf
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-10 14:23:26 -0800

    Added task 15

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Provide a function to slice a numpy array along specific axes.
+"""
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """
+    Slice a numpy array along specific axes.
+    """
+    slices = (slice(*axes.get(depth, (None, None)))
+                    for depth in range(len(matrix.shape)))
+    return matrix[tuple(slice)]
