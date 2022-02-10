ea2fb8b2213f1189d3d28d5bd36b4d4fc59457ee
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-10 14:08:06 -0800

    Fixed task 13

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -9,4 +9,4 @@ def np_cat(mat1, mat2, axis=0):
     """
     Concatenate two numpy arrays along a specific axis.
     """
-    return np.concatenate(mat1, mat2, axis=axis)
+    return np.concatenate((mat1, mat2), axis=axis)
