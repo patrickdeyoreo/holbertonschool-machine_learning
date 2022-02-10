a265b07c4e66f82f7b6c03659aac0ce7c598149d
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-09 23:59:43 -0800

    Added task 10

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Provide a function to calculates the shape of an array.
+"""
+import numpy as np
+
+def np_shape(matrix: np.array) -> tuple:
+    """
+    Calculate the shape of an array.
+    """
+    return matrix.shape
