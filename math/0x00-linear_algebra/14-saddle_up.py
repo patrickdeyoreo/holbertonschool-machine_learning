914f1c53ffc92ff021bfd97fe2d5df233b5645af
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-10 14:14:13 -0800

    Using np.matmul

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -2,10 +2,11 @@
 """
 Provide a function to perform matrix multiplication of two numpy arrays.
 """
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """
     Perform matrix multiplication of two numpy arrays.
     """
-    return mat1 @ mat2
+    return np.matmul(mat1, mat2)
