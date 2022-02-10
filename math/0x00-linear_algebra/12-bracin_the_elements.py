08e4c692257e7ff2373251dd5f47b8c0ffd0502f
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-10 00:15:41 -0800

    Added task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Provide a function that adds, subtracts, multiplies and divides numpy arrays.
+"""
+
+def np_elementwise(mat1, mat2):
+    """
+    Add, subtract, multiply and divide numpy arrays.
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
