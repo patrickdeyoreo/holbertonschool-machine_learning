781b4cc5fc3dcbddb4e79f2f05ffc3b028354a7c
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-21 20:07:51 -0800

    Added task 6

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Provides a function to concatenate two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays"""
+    return arr1 + arr2
