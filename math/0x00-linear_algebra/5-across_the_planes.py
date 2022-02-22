aa695d539070c16c0bc80f7a313ceb6ac58f4a13
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-21 20:06:58 -0800

    Added task 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Provides a function to perform element-wise additon of two 2D matrices"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Performs element-wise additon of two 2-dimensional matrices"""
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        return [[a + b for a, b in zip(u, v)] for u, v in zip(mat1, mat2)]
+    return None
