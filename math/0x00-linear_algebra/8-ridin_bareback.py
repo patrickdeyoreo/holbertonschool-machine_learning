bd2769960a6d20233e0c14642d73083bbf7038bd
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-21 20:09:34 -0800

    Added task 8

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Provides a function to muliply two 2D matrices"""
+
+
+def mat_mul(mat1, mat2):
+    """Performs multiplication of two 2-dimensional matrices"""
+    if len(mat1[0]) == len(mat2):
+        return [[sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)]
+                for row in mat1]
+    return None
