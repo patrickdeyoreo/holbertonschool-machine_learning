4b51b6486c59bd2fc91842635e99675e8f3c8a9b
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-03-02 14:20:12 -0800

    Validating nodes in task 8

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -25,6 +25,14 @@ class NeuralNetwork:
           ValueError:
             argument 'nx' is less than 1, or argument 'nodes' is less than 1
         """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
