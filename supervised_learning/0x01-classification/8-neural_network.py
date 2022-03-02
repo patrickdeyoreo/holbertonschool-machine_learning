64ecd7162340786c44f39c9964996fae1a063f12
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-03-02 14:14:11 -0800

    Added task 8

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Provide a class NeuralNetwork that defines a neural network with one hidden
+layer for performing binary classification.
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Define a neural network for performing binary classification.
+    """
+    def __init__(self, nx, nodes):
+        """
+        Initialize a neural network.
+        Args:
+          nx (int):
+            the number of inputs to the neuron
+          nodes(int):
+            the number of nodes in the network
+        Raises:
+          TypeError:
+            argument 'nx' is not an integer, or argument 'nodes' is not an
+            integer
+          ValueError:
+            argument 'nx' is less than 1, or argument 'nodes' is less than 1
+        """
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
